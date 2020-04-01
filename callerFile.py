bdf83e8bfd8f181ccab346902f85324945184327
NeighbourLizard <giorgi.artsivadze@gmail.com>
2020-04-02 01:43:36 +0300

    version 0.0.5

--- callerFile.py
+++
@@ -1,6 +0,0 @@
-import quadratic_equation_giarts
-a = float(input("Please input a: "))
-b = float(input("Please input b: "))
-c = float(input("Please input c: "))
-quadratic_equation_giarts.quadratic_equation_giarts(a,b,c)
-exit()
